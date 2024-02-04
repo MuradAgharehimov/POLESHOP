616a8958a0c2cbccf26adece269e689f97587ec0
MuradAgharehimov <146194941+MuradAgharehimov@users.noreply.github.com>
2024-02-04 21:45:47 +0400

    COMMIT 04

---
+++ assets/js/add.js
@@ -0,0 +1,107 @@
+const proList = document.getElementById('products')
+const titleInp = document.getElementById('titleInp')
+const priceInp = document.getElementById('priceInp')
+const imageInp = document.getElementById('imageInp')
+const myForm = document.getElementById('myForm')
+const axtarInp = document.getElementById('axtar')
+const axtarBtn = document.getElementById('axtarBtn')
+const az = document.getElementById('az')
+const za = document.getElementById('za')
+const defa = document.getElementById('defa')
+
+
+function postProduct (e) {
+    e.preventDefault()
+    axios.post('https://65675cba64fcff8d73103f34.mockapi.io/xpolee', {
+        title: titleInp.value,
+        price: priceInp.value,
+        image: imageInp.value
+    })
+    .then( res => {
+        getData()
+        myForm.reset()
+    })
+}
+
+myForm.addEventListener('submit', postProduct)
+
+function getData () {
+    proList.innerHTML = ``
+    axios.get('https://65675cba64fcff8d73103f34.mockapi.io/xpolee')
+    .then( res => {
+        db = res.data
+        db.map(item => {
+            const box = document.createElement('tr')
+            box.innerHTML = ` <td>${item.title}</td> 
+             <td>${item.price}</td> 
+             <button onclick="deleteItem(${item.id})">Delete</button>
+             `
+             proList.appendChild(box)
+        })
+    })
+}
+getData()
+
+
+function deleteItem (id) {
+    axios.delete(`https://65675cba64fcff8d73103f34.mockapi.io/xpolee/${id}`)
+    .then(res => {
+        getData()
+    })
+}
+
+function getByName () {
+    proList.innerHTML = ``
+    axios.get('https://65675cba64fcff8d73103f34.mockapi.io/xpolee')
+    .then(res => {
+        db = res.data
+        let filteredData = db.filter(item => item.title.toLowerCase().startsWith(axtarInp.value.toLowerCase()))
+        filteredData.map(item => {
+            const box = document.createElement('tr')
+            box.innerHTML = ` <td>${item.title}</td> 
+             <td>${item.price}</td> 
+             <button onclick="deleteItem(${item.id})">Delete</button>
+             `
+             proList.appendChild(box)
+        })
+    })
+}
+
+axtarBtn.addEventListener('click', getByName)
+
+function sortAz () {
+    proList.innerHTML = ``
+    axios.get('https://65675cba64fcff8d73103f34.mockapi.io/xpolee')
+    .then(res => {
+        db = res.data
+        let filteredData = db.sort((a, b) => a.title.localeCompare(b.title))
+        filteredData.map(item => {
+            const box = document.createElement('tr')
+            box.innerHTML = ` <td>${item.title}</td> 
+             <td>${item.price}</td> 
+             <button onclick="deleteItem(${item.id})">Delete</button>
+             `
+             proList.appendChild(box)
+        })
+    })
+}
+function sortZa () {
+    proList.innerHTML = ``
+    axios.get('https://65675cba64fcff8d73103f34.mockapi.io/xpolee')
+    .then(res => {
+        db = res.data
+        let filteredData = db.sort((a, b) => b.title.localeCompare(a.title))
+        filteredData.map(item => {
+            const box = document.createElement('tr')
+            box.innerHTML = ` <td>${item.title}</td> 
+             <td>${item.price}</td> 
+             <button onclick="deleteItem(${item.id})">Delete</button>
+             `
+             proList.appendChild(box)
+        })
+    })
+}
+
+az.addEventListener('click', sortAz)
+za.addEventListener('click', sortZa)
+defa.addEventListener('click', getData)
